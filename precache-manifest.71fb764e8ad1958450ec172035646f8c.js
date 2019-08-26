a585c577c9ee9f19ad95228c82658e0e77f639e0
Dima <bid93@mail.ru>
2019-08-26 16:55:47 +0300

    Updates

---
+++ precache-manifest.71fb764e8ad1958450ec172035646f8c.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75d9e584333e71574e9d850776763729",
+    "url": "/business-card/index.html"
+  },
+  {
+    "revision": "afd7417044f0eb92c81f",
+    "url": "/business-card/static/css/main.acc274cb.chunk.css"
+  },
+  {
+    "revision": "4b1773c72d85256bad9d",
+    "url": "/business-card/static/js/2.00ccda25.chunk.js"
+  },
+  {
+    "revision": "afd7417044f0eb92c81f",
+    "url": "/business-card/static/js/main.841bbed1.chunk.js"
+  },
+  {
+    "revision": "5bce175fe2c935cac40b",
+    "url": "/business-card/static/js/runtime~main.9ff25d6e.js"
+  },
+  {
+    "revision": "11904fa98a0be257494524b955498e0d",
+    "url": "/business-card/static/media/avatar.11904fa9.png"
+  },
+  {
+    "revision": "62711ea5999eb1c10d505f18873a2049",
+    "url": "/business-card/static/media/babel.62711ea5.png"
+  },
+  {
+    "revision": "5209029d9446783a2472fa38a59cdcc8",
+    "url": "/business-card/static/media/bootstrap4.5209029d.png"
+  },
+  {
+    "revision": "3f8e68f49f7dba3390a43f8fcc1a14f7",
+    "url": "/business-card/static/media/css3.3f8e68f4.png"
+  },
+  {
+    "revision": "2ea01c0e92883b6e481dd2e62f5e6d4e",
+    "url": "/business-card/static/media/html5.2ea01c0e.png"
+  },
+  {
+    "revision": "133610437ccf8e517abd435bcff2e795",
+    "url": "/business-card/static/media/node.13361043.png"
+  },
+  {
+    "revision": "47ce6e77f039020ee2e76a10c1e988e9",
+    "url": "/business-card/static/media/react.47ce6e77.png"
+  },
+  {
+    "revision": "59c467536111a981d904a050d335c2be",
+    "url": "/business-card/static/media/redux.59c46753.png"
+  },
+  {
+    "revision": "e051f1a2711a770c526c1d48768c606a",
+    "url": "/business-card/static/media/webpack.e051f1a2.png"
+  }
+]);
